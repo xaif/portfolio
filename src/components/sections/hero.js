26b206bccef5e8754ed8072953d59de2c90bf515
= <=>
2023-09-02 01:46:08 +0530

    Finishing the fold!

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -60,16 +60,16 @@ const Hero = () => {
   }, []);
 
   const one = <h1>Hi, my name is</h1>;
-  const two = <h2 className="big-heading">Huzaifa Dhapai.</h2>;
+  const two = <h2 className="big-heading">Huzaifa Dhapai</h2>;
   const three = <h3 className="big-heading">I build things for the web.</h3>;
   const four = (
     <>
       <p>
-        I’m a software engineer specializing in building (and occasionally designing) exceptional
-        digital experiences. Currently, I’m focused on building accessible, human-centered products
+        I’m a tech product manager specializing in building (and occasionally designing) exceptional
+        digital experiences. Currently, I’m focused on building, a web hosting company & Learning Management System
         at{' '}
-        <a href="https://upstatement.com/" target="_blank" rel="noreferrer">
-          Upstatement
+        <a href="https://webverge.io/" target="_blank" rel="noreferrer">
+          WebVerge
         </a>
         .
       </p>
@@ -78,10 +78,10 @@ const Hero = () => {
   const five = (
     <a
       className="email-link"
-      href="https://www.newline.co/courses/build-a-spotify-connected-app"
+      href="https://webverge.io"
       target="_blank"
       rel="noreferrer">
-      Check out my course!
+      Check out my Resume!
     </a>
   );
 
