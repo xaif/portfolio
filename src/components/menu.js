af843a828cfd58d6f223e500821df6cc0c2c035e
Xaif <huzaifaamirka46@gmail.com>
2024-08-24 15:41:15 +0530

    Added new link for resume & made it trainer profile

--- src/components/menu.js
+++ src/components/menu.js
@@ -266,8 +266,8 @@ const Menu = () => {
               </ol>
             )}
 
-            <a href="/resume.pdf" className="resume-link">
-              Resume
+            <a href="/trainer-profile.pdf" className="resume-link">
+              Trainer Profile
             </a>
           </nav>
         </StyledSidebar>
