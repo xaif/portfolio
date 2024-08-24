2ec233d87cf6801174bb2a72bbc2dbe4d84e1b9d
Xaif <huzaifaamirka46@gmail.com>
2024-08-24 15:48:16 +0530

    Changed nav bar

--- src/components/nav.js
+++ src/components/nav.js
@@ -206,8 +206,12 @@ const Nav = ({ isHome }) => {
   );
 
   const ResumeLink = (
-    <a className="resume-button" href="/resume.pdf" target="_blank" rel="noopener noreferrer">
-      Resume
+    <a
+      className="resume-button"
+      href="/trainer-profile.pdf"
+      target="_blank"
+      rel="noopener noreferrer">
+      Trainer Profile
     </a>
   );
 
