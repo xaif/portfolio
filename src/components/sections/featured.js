558d7c4b6f7ef4d16c0fe8f3937c1fbb29c2db09
Xaif <huzaifaamirka46@gmail.com>
2024-08-26 17:38:28 +0530

    Footer & Featured tag

--- src/components/sections/featured.js
+++ src/components/sections/featured.js
@@ -348,7 +348,7 @@ const Featured = () => {
   return (
     <section id="projects">
       <h2 className="numbered-heading" ref={revealTitle}>
-        Some Things I’ve Built
+        Things I've Done
       </h2>
 
       <StyledProjectsGrid>
